b3a9516385e6b69ff2a221322ce86e2ab91372d1
Ulyana Bychkovskaya <ubychkovskaya20@gmail.com>
2019-10-27 17:30:20 +0300

    feat: add matrix32

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
+import './style.css';
 import * as matrix32 from './assets/data/32x32.json'; 
 import * as matrix4 from './assets/data/4x4.json'; 
-
-console.log(matrix32.default[0][1]);
 
 let file = document.querySelectorAll('.files-panel__file');
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
+let jsonData1 = matrix4.default; 
+let jsonData2 = matrix32.default;
+
+for(let i = 0; i < jsonData2.length; i++) {
+    for(let j = 0; j < jsonData2[0].length; j++) {
+        jsonData2[i][j] = RGBAToHex(jsonData2[i][j][0], jsonData2[i][j][1], jsonData2[i][j][2]);
+    }
+}
 
 [].forEach.call(file, function(elem) {
     elem.onclick = function() {
-    
+
+        for(let elem of file) {
+            elem.style.background = '#ffffff';
+        }
+
         if(this.innerText === 'image.png') {
             drawImage();
+            document.querySelector('.file-3').style.background = '#e6e6e6';
         }
         if(this.innerText === '4x4.json') {
-            /*let jsonData = [
-                ["00BCD4", "FFEB3B","FFEB3B","00BCD4"],
-                ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-                ["FFEB3B", "FFC107","FFC107","FFEB3B"],
-                ["00BCD4", "FFEB3B","FFEB3B","00BCD4"]
-            ];*/
-            let jsonData = matrix4.default; 
-            drawJSON(jsonData);
+            drawJSON(jsonData1);
+            document.querySelector('.file-1').style.background = '#e6e6e6';
         }
         if(this.innerText === '32x32.json') {
-
+            drawJSON(jsonData2);
+            document.querySelector('.file-2').style.background = '#e6e6e6';
         }
+
     }
 });
 
@@ -50,11 +58,10 @@ function drawJSON(data) {
     }
 }
 
-function RGBAToHex(r,g,b,a) {
+function RGBAToHex(r,g,b) {
     r = r.toString(16);
     g = g.toString(16);
     b = b.toString(16);
-    a = Math.round(a * 255).toString(16);
   
     if (r.length == 1)
       r = "0" + r;
@@ -62,40 +69,6 @@ function RGBAToHex(r,g,b,a) {
       g = "0" + g;
     if (b.length == 1)
       b = "0" + b;
-    if (a.length == 1)
-      a = "0" + a;
-  
-    return "#" + r + g + b + a;
-  }
-/*file.onclick = function() {
-    console.log(this.innerText);
-}*/
 
-
-/*file[0].onclick = function() {
-    console.log("hiii");
-    var img = new Image();
-
-    img.onload = function() {
-        context.drawImage(img, 0, 0, 512, 512);
-    };
-    img.src = "./assets/data/image.png";
+    return r + g + b;
 }
-file[1].onclick = function() {
-    console.log("hiii");
-    var img = new Image();
-
-    img.onload = function() {
-        context.drawImage(img, 0, 0, 512, 512);
-    };
-    img.src = "./assets/data/image.png";
-}
-file[2].onclick = function() {
-    console.log("hiii");
-    var img = new Image();
-
-    img.onload = function() {
-        context.drawImage(img, 0, 0, 512, 512);
-    };
-    img.src = "./assets/data/image.png";
-} */
